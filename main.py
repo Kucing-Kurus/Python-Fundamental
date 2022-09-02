eaf5c868c8458cba556345d07cf1bd525f0c6fb3
HP <inh.iyas97@gmail.com>
2022-09-02 13:41:37 +0700

    sekuensional.py

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+semua sintaksis dasar bahasa pemrogaman terdiri dari :
+1. Sekuensional : langkah berurutan
+2. Percabangan : langkah melompat jika kondisi mempenuhi
+3. Perulangan : mengulang langkah berkali kali selama / sampai kondisi terpenuhi
+'''
+## Sekuensial
+print('Ibu berkata,"Pergi ke toko"')
+print('Budi menjawab,"Baik, apa yang harus saya lakukan di tokok ?"')
+print('Ibu menjawab,"Beli 1 botol susu, dan jika ada telur beli 6"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
